1443f3f6eaef33e75ac083f7017aa636d5ac528b
Jeannie An <jeanniehjan@gmail.com>
2020-10-17 01:13:14 +0100

    implemented heap

---
+++ AStar/src/Logic/Heap.h
@@ -0,0 +1,32 @@
+#pragma once
+template <typename T>
+struct HeapItem {
+	int index = 0;
+	T item;
+};
+
+template <typename T>
+class Heap
+{
+public:
+	HeapItem items[];
+	int count;
+
+	Heap() = default;
+
+	void Add(HeapItem<T> item);
+	void Balance(HeapItem<T> item);
+};
+
+template<typename T>
+inline void Heap<T>::Add(HeapItem<T> item)
+{
+	item.index = count;
+	items[count++] = item;
+	Balance(item);
+}
+
+template<typename T>
+inline void Heap<T>::Balance(HeapItem<T> item)
+{
+}
